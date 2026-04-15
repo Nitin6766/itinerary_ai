060a85880f717bf49d8a3eec573106aec5291b7b
Nitin6766 <pandeynitin0436@gmail.com>
2026-04-15 23:30:41 +0530

    clean repo (no api key)

---
+++ README.md
@@ -0,0 +1,39 @@
+# 🌍 AI Travel Itinerary Generator
+
+An AI-powered travel planner that generates day-wise itineraries based on your budget, location, and trip duration.
+
+---
+
+## 🚀 Features
+
+- 🗺️ Location-based travel plans
+- 💰 Budget-friendly suggestions
+- 📅 Day-wise itinerary
+- 🍽️ Food recommendations
+- 🤖 AI-generated travel guide
+
+---
+
+## 🛠️ Tech Stack
+
+### Frontend
+
+- React (Vite + TypeScript)
+- Axios
+
+### Backend
+
+- FastAPI (Python)
+- Groq API (LLaMA model)
+
+---
+
+## 📦 Installation
+
+### 🔹 Backend
+
+```bash
+cd backend
+pip install fastapi uvicorn requests
+uvicorn main:app --reload
+```
---
+++ app.py
@@ -0,0 +1,63 @@
+import requests
+import time
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app=FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+API_KEY="your api key"
+url="https://api.groq.com/openai/v1/chat/completions"
+
+headers={
+  "Authorization":f"Bearer {API_KEY}",
+  "Content-Type":"application/json"
+}
+chat_history={}
+@app.get("/ai")
+async def bhakol(budget:str,location:str,days:int,user_id:int):
+  prompt = f"""
+    Create a detailed travel itinerary:
+    Location: {location}
+    Budget: {budget}
+    Days: {days}
+
+    Include:
+    - Day wise plan
+    - Places to visit
+    - Food suggestions
+    - Budget breakdown
+    """
+
+  if user_id not in chat_history:
+    chat_history[user_id]=[]
+  chat_history[user_id].append({"role":"user",
+                                "content":prompt})
+  data={"model":"llama-3.3-70b-versatile",
+        "messages":chat_history[user_id]}
+  
+  max_entries=3
+  for i in range(max_entries):
+    response=requests.post(url,headers=headers,json=data)
+
+    if response.status_code==200:
+      break
+    elif response.status_code==429:
+      time.sleep(3)
+    else:
+      return {"error":response.status_code}
+  result=response.json()
+  if "choices" in result:
+    answer=result["choices"][0]["message"]["content"]
+    chat_history[user_id].append({"role":"assistant","content":answer})
+    return {"answer":answer}
+  return {"error":result}
